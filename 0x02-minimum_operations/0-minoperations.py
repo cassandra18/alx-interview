ad4159a034094d23fbc9b42daa14dc97a3414d8a
cassandra18 <cassandralelei013@gmail.com>
2024-08-20 23:24:29 +0300

    initial commit

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+def minOperations(n: int) -> int:
+    """
+    Calculate the minimum number of operations needed
+    to achieve n H characters.
+
+    Args:
+        n (int): The number of H characters desired.
+
+    Returns:
+        int: The minimum number of operations required,
+        or 0 if n is impossible.
+    """
+    if n <= 1:
+        return 0
+
+    operations = 0
+    factor = 2
+
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+
+    return operations
